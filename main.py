540914ceaf8af19d2ffeda48cabab155faa5aabe
Alexander Schulze <cibernop@protonmail.com>
2021-05-24 10:06:22 +0200

    write out result to file and stdout

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import json
+import logging
+import pathlib
 import subprocess
 from datetime import datetime, time
+
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT)
+
+logger = logging.getLogger('speedtest')
 
 
 def measure_speed():
@@ -9,13 +16,31 @@ def measure_speed():
     output = json.loads(process_result.stdout)
     timestamp = datetime.strptime(output['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
     return {
-        'download': output['download'],
-        'upload': output['upload'],
+        'download': output['download'] / (1000**2),
+        'upload': output['upload'] / (1000**2),
         'ping': output['ping'],
         'date': timestamp.date(),
         'time': timestamp.time(),
     }
 
 
+def protocol_measure(file: pathlib.Path):
+    result = measure_speed()
+
+    if (file.exists()):
+        file_contents = json.loads(file.read_text())
+    else:
+        file_contents = []
+
+    file_contents.append(result)
+    file.write_text(json.dumps(file_contents, default=str))
+
+    logger.info('Download: %.2f Mbps, Upload: %.2f Mbps, Ping: %.1f',
+                result['download'], result['upload'], result['ping'])
+
+
 if __name__ == "__main__":
-    measure_speed()
+    logger.setLevel(logging.INFO)
+
+    file = pathlib.Path('result.json')
+    protocol_measure(file)
